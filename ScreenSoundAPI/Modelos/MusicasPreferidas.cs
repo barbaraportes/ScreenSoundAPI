d7248d317a932fd0e73776620fbbca15c7ef28c6
Bárbara Portes Braga <bportesb@gmail.com>
2023-12-14 18:21:58 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSoundAPI/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+
+namespace ScreenSoundAPI.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+
+    public List<Musica> listaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome) {
+
+        Nome = nome;
+        listaDeMusicasFavoritas = new List<Musica>();
+
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        listaDeMusicasFavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Essas são as músicas favoritas de {Nome}:\n");
+        foreach(var musica in listaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+        }
+        Console.WriteLine();
+    }
+
+    public void GerarArquivoJson() {
+
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = listaDeMusicasFavoritas
+        });
+
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"O arquivo Json foi criado com sucesso! {Path.GetFullPath(nomeDoArquivo)}");
+    }
+
+
+
+}
---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+using ScreenSoundAPI.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        //Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //musicas[1].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+        //LinqFilter.FiltrarMusicasPeloAno(musicas, 2012);
+        //LinqFilter.FiltrarMusicasPorTonalidade(musicas, "C#");
+
+        
+        //var musicasPreferidasDaBarbara = new MusicasPreferidas("Barbara");
+        //musicasPreferidasDaBarbara.AdicionarMusicasFavoritas(musicas[500]);
+        //musicasPreferidasDaBarbara.AdicionarMusicasFavoritas(musicas[637]);
+        //musicasPreferidasDaBarbara.AdicionarMusicasFavoritas(musicas[428]);
+        //musicasPreferidasDaBarbara.AdicionarMusicasFavoritas(musicas[13]);
+        //musicasPreferidasDaBarbara.AdicionarMusicasFavoritas(musicas[71]);
+
+        //musicasPreferidasDaBarbara.ExibirMusicasFavoritas();
+
+        //musicasPreferidasDaBarbara.GerarArquivoJson();
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+
+    
+}
